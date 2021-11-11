44e59af79252d674b770f895236511c39c5e3eaf
Oscary13 <oscary_1320114020@uptecamac.edu.mx>
2021-11-11 17:18:35 -0600

    edits

--- administrador/config/bd.php
+++ administrador/config/bd.php
@@ -1,8 +1,8 @@
 <?php
-$host="localhost";
-$bd="sitio";
-$usuario="root";
-$contrasenia="";
+$host="s29oj5odr85rij2o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$bd="p2atf3l0jrdmp6vu";
+$usuario="tv5iiwt0r3m4sumg";
+$contrasenia="gnnco1kqe10ked3q";
 try {
     $conexion=new PDO("mysql:host=$host;dbname=$bd",$usuario,$contrasenia);
 } catch (Exception $ex) {
