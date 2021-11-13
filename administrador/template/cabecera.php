4cab9ad669f4ddd8374d1f3223c30b9c2cc8f159
Oscary13 <oscary_1320114020@uptecamac.edu.mx>
2021-11-13 12:19:46 -0600

    edits

--- administrador/template/cabecera.php
+++ administrador/template/cabecera.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION['usuario'])) {
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="../../logo.ico">
+    <link rel="icon" href="../logo.ico">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/app.css">
    </head>
